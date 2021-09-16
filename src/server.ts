4509885b8d3704db6c4764c79475399e901c20b8
Diego Sousa <diegointeriores11@gmail.com>
2021-09-16 18:46:38 -0300

    refactor: add validation for port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { router } from './routes';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(router);
 app.listen(port, () => {
