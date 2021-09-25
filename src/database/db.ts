98f6a869863b119261509a1ef4f9bb4b292481e0
Diego Sousa <diegointeriores11@gmail.com>
2021-09-25 12:06:35 -0300

    fix pgp connection

--- src/database/db.ts
+++ src/database/db.ts
@@ -6,7 +6,19 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME,
 } = process.env;
 
-const pgp = pgPromise({});
-const connectionString = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const ssl = process.env.NODE_ENV !== 'production' ? false : {
+  rejectUnauthorized: false,
+};
 
-export const db = pgp(connectionString);
+const pgp = pgPromise({});
+const config = {
+  host: DB_HOST,
+  port: +DB_PORT,
+  database: DB_NAME,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  max: 30,
+  ssl,
+};
+
+export const db = pgp(config);
